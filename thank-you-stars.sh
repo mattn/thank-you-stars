1174ff43f1261969f76a9290b1d213adf58c0de5
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2017-08-26 23:27:47 +0900

    filter github repos

--- thank-you-stars.sh
+++ thank-you-stars.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 go get github.com/mattn/thank-you-stars/github-star
-go list -f '{{join .Deps "\n"}}' |  xargs go list -f '{{if not .Standard}}{{.ImportPath}}{{end}}' | xargs -n 1 github-star
+go list -f '{{join .Deps "\n"}}' |  xargs go list -f '{{if not .Standard}}{{.ImportPath}}{{end}}' | grep '^github\.com' | xargs -n 1 github-star
